35e9e6fae482e0a7408d3d2f6752b7b3d32e96e0
Nguyen Ha Hoang Anh <anh.nguyenha10@mobifone.vn>
2025-12-26 17:39:18 +0700

    fix model

--- src/main/java/com/example/footballhub_admin_backend/model/Competition.java
+++ src/main/java/com/example/footballhub_admin_backend/model/Competition.java
@@ -5,8 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @Entity
 @Data
 @NoArgsConstructor
